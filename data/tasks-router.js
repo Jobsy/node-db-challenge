6671e61378aa519541636d551759095af9f3f502
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 12:38:24 +0100

    add tasks-router get(/) / resources-router.js

--- data/tasks-router.js
+++ data/tasks-router.js
@@ -3,3 +3,16 @@ const express = require("express");
 const dB = require("./db");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    dB.findTasks()
+        .then((tasks) => {
+            res.status(200).json(tasks)
+        })
+        .catch((err) => {
+            res.status(500).json({ error: "The tasks information could not be retrieved." + err })
+        })
+})
+
+
+module.exports = router;
