9ac5493bd0b07956742c69d97b5f8a701894b9fa
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 11:35:57 +0100

    install and import express and dotenv / project set-up / package.json

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const router = require("./data/cars-router");
+const router = require("./data/projects-router");
 
 server.use(express.json());
 server.use(cors());
-server.use("/api/cars", router);
+server.use("/api/projects", router);
 
 server.get("/", (req, res) => {
     res.send("hello world");
