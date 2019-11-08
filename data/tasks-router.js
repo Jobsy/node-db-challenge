9d31829ec07ec345790168fb551a071664e4ffb0
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 12:42:20 +0100

    add tasks-router get(:id) / resources-router.js

--- data/tasks-router.js
+++ data/tasks-router.js
@@ -14,5 +14,21 @@ router.get("/", (req, res) => {
         })
 })
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const { url } = req;
+
+    dB.findTaskById(id)
+        .then((tasks) => {
+
+            if (tasks.length === 0) {
+                res.status(404).json({ message: "The task with the specified ID does not exist." })
+            }
+            res.status(200).json({ resourceInfo: tasks, url: url, operation: "GET"  })
+        })
+        .catch((err) => {
+            res.status(500).json({ error: "The task information could not be retrieved." + err })
+        })
+})
 
 module.exports = router;
