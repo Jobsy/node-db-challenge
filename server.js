7b0147330278331873e4f17da1da1f0051cc75c9
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 12:10:45 +0100

    update server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const router = require("./data/projects-router");
+const router2 = require("./data/resources-router");
 
 server.use(express.json());
 server.use(cors());
 server.use("/api/projects", router);
+server.use("/api/resources", router2);
 
 server.get("/", (req, res) => {
     res.send("hello world");
