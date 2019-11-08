740dab1916402318b616520d6ab9272210c1f6c7
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 09:33:12 +0100

    update export.up()

--- data/migrations/20191108091746_create-table.js
+++ data/migrations/20191108091746_create-table.js
@@ -1,8 +1,38 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("projects", table => {
+            table.increments()
+            table.string("project_name", 128)
+                .unique()
+                .notNullable()
+        })
+        .createTable("resources", table => {
+            table.increments()
+            table.string("resource_name", 128)
+                .notNullable()
+        })
+        .createTable("tasks", table => {
+            table.increments()
+            table.string("task_name", 128)
+            table.integer("project_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("projects")
+            // .onUpdate("CASCADE")
+            // .onDelete("CASCADE")
+            table.integer("resource_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("resources")
+                // .onUpdate("CASCADE")
+                // .onDelete("CASCADE")
+            // table.primary(["recipe_id", "ingredient_id"])
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
