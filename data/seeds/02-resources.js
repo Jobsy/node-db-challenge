29a2714d78914a511f414b02c613b6bb977cafef
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 10:55:21 +0100

    create and update 02-resources.js

---
+++ data/seeds/02-resources.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex) {
+  return knex('resources').insert([
+    {resource_name: 'butter', resource_description: 'making butter'},
+    {resource_name: 'butter2222', resource_description: 'making butter22222'},
+    {resource_name: 'butter3333', resource_description: 'making butter33333'},
+  ]);
+};
