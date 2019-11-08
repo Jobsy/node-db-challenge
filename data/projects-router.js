b79b118c4e187bdd91e3ebe831f240017cec49a6
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 12:00:09 +0100

    add projects-router get(:id) / projects-router.js

--- data/projects-router.js
+++ data/projects-router.js
@@ -19,5 +19,23 @@ router.get("/", (req, res) => {
         })
 })
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const { url } = req;
+
+    dB.findById(id)
+        .then((projects) => {
+
+            if (projects.length === 0) {
+                res.status(404).json({ message: "The project with the specified ID does not exist." })
+            }
+            res.status(200).json({ projectInfo: projects, url: url, operation: "GET"  })
+        })
+        .catch((err) => {
+            res.status(500).json({ error: "The project information could not be retrieved." + err })
+        })
+})
+
+
 
 module.exports = router;
